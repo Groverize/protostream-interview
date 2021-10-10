1a0133279cff9e31a7e860512e142640226f03b7
Ian Grover (home) <iangrover63@gmail.com>
2021-10-10 07:54:05 +0100

    Implement simple login form. Add bootstrap (via cdn)

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required]
+  })
+
+  constructor(private fb: FormBuilder,
+              private auth: AuthService, 
+              public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.auth.authenticate();
+    this.router.navigate(['home']);
   }
 
 }
